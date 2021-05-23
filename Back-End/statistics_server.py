69c52c5c6477cc0bdb752b0554ac888efd77894e
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-05-23 17:39:17 -0400

    specified POST and GET requests in flask and added CORS policy

--- Back-End/statistics_server.py
+++ Back-End/statistics_server.py
@@ -1,24 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from statistics import Statistics
+from flask_cors import CORS
 
 
 class FlaskServer:
 
     def __init__(self):
         self.service = Flask(__name__)
+        CORS(self.service, resources={"/*": {"origins": ["*"]}})
         self.service.add_url_rule('/', view_func=self.home)
-        self.service.add_url_rule('/results', view_func=self.results)
+        self.service.add_url_rule('/results', view_func=self.results, methods=['POST', 'GET'])
         self.service.run()
 
     def home(self):
         return "HOME"
 
     def results(self):
-        data = {"name": "Samaneh"}
-        stats = Statistics(data, "json")
+        print(request.headers)
+        print(request.data)
+
+        stats = Statistics(request.data, "json")
         return stats.formatted_response()
 
 
 if __name__ == '__main__':
     server = FlaskServer()
-
