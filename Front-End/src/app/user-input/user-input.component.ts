5dceb1071580cf1a8b59e2a8afd26b726a0431b8
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-05-26 14:42:40 -0400

    show spinner if we have delay in receiving data from back end and then navigate to the statistics page

--- Front-End/src/app/user-input/user-input.component.ts
+++ Front-End/src/app/user-input/user-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {StatisticsService} from '../statistics.service';
 import {Router} from '@angular/router';
 import {NgxSpinnerService} from 'ngx-spinner';
@@ -12,15 +12,17 @@ import {NgxSpinnerService} from 'ngx-spinner';
 export class UserInputComponent implements OnInit {
   form: FormGroup;
   jsonString;
+  responseData;
 
   constructor(private service: StatisticsService, public router: Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      inputData: new FormControl(''),
+      inputData: new FormControl('', [Validators.required]),
       uploadFile: new FormControl('')
     });
   }
+  get inputData() { return this.form.get('inputData'); }
   onFileSelected(event) {
     const fileReader = new FileReader();
     fileReader.readAsText(event.target.files[0], 'UTF-8');
@@ -37,23 +39,23 @@ export class UserInputComponent implements OnInit {
 
   onSubmit(){
     if (this.form.value.inputData) {
-      console.log('this is coming from inputData');
+      this.spinner.show();
       const requestObserver = this.service.uploadJson(this.form.value.inputData);
       requestObserver.subscribe({
         next: data => {
+          // remove spinner
+          this.spinner.hide();
+          this.router.navigate(['/results']);
+          // route to next page and pass data
+          this.responseData = data;
           console.log('## RESPONSE FROM BACK END ##', data);
         },
         error: error => {
+          this.spinner.hide();
           const errorMessage = error.message;
           console.error('There was an error!', error);
         }
       });
     }
-    this.form.reset();
-    this.spinner.show();
-    setTimeout(() => {
-      this.spinner.hide();
-      this.router.navigate(['/results']);
-    }, 5000);
   }
 }
