4e668732b7e23d5e6a9a2f5265498c39956fb374
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-05-27 02:13:25 -0400

    data displayed as a chart

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserInputComponent } from './user-input/user-input.component';
@@ -10,13 +9,14 @@ import { StatisticsResultComponent } from './statistics-result/statistics-result
 import {StatisticsService} from './statistics.service';
 import {NgxSpinnerModule} from 'ngx-spinner';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgApexchartsModule} from 'ng-apexcharts';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserInputComponent,
-    StatisticsResultComponent
+    StatisticsResultComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,6 +24,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     HttpClientModule,
     AppRoutingModule,
     NgxSpinnerModule,
+    NgApexchartsModule,
     BrowserAnimationsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- Front-End/src/app/statistics-result/statistics-result.component.ts
+++ Front-End/src/app/statistics-result/statistics-result.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {UserInputComponent} from '../user-input/user-input.component';
 import {StatisticsService} from '../statistics.service';
+import {ApexChart, ApexNonAxisChartSeries, ApexResponsive, NgApexchartsModule} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-statistics-result',
   templateUrl: './statistics-result.component.html',
   styleUrls: ['./statistics-result.component.css']
 })
-export class StatisticsResultComponent implements OnInit {
+export class StatisticsResultComponent{
+  @ViewChild('chart-stats') chart: StatisticsResultComponent;
+  public chartOptions: Partial<ChartOptions>;
   statistics;
   definitionsD;
-
   statisticOne;
   statisticTwo;
   statisticThree;
@@ -21,41 +30,80 @@ export class StatisticsResultComponent implements OnInit {
   constructor(private userInput: UserInputComponent, private service: StatisticsService) {
     this.statistics = this.service.responseData.stats;
     this.definitionsD = this.service.responseData.definitions;
+
+    this.chartOptions = {
+      series: [],
+      chart: {
+        width: 500,
+        type: 'pie'
+      },
+      labels: [],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }
+      ]
+    };
   }
-
-  ngOnInit(): void {
-
-  }
-
   statOne(){
     this.statisticOne = this.statistics.percent_stat_1;
-    return this.statisticOne;
+    this.chartOptions.series = [this.statisticOne, (100 - this.statisticOne)];
   }
-
   statTwo(){
     this.statisticTwo = this.statistics.percent_stat_2;
-    return this.statisticTwo;
+    this.chartOptions.series = [this.statisticTwo, (100 - this.statisticTwo)];
   }
   statThree(){
     this.statisticThree = this.statistics.percent_stat_3;
-    return this.statisticThree;
+    this.chartOptions.series = [this.statisticThree, (100 - this.statisticThree)];
   }
   statFour(){
     this.statisticFour = this.statistics.percent_stat_4;
-    return this.statisticFour;
+    const arr = [];
+    for (const key in this.statisticFour){
+      if (this.statisticFour.hasOwnProperty(key)){
+        arr.push(this.statisticFour[key]);
+      }
+    }
+    this.chartOptions.series = arr;
   }
   statFive(){
     this.statisticFive = this.statistics.percent_stat_5;
-    return this.statisticFive;
+    const arr = [];
+    for (const key in this.statisticFive){
+      if (this.statisticFive.hasOwnProperty(key)){
+        arr.push(this.statisticFive[key]);
+      }
+    }
+    this.chartOptions.series = arr;
   }
   statSix(){
     this.statisticSix = this.statistics.percent_stat_6;
-    return this.statisticSix;
+    const arr = [];
+    for (const key in this.statisticSix){
+      if (this.statisticSix.hasOwnProperty(key)){
+        arr.push(this.statisticSix[key]);
+      }
+    }
+    this.chartOptions.series = arr;
   }
   statSeven(){
     this.statisticSeven = this.statistics.percent_stat_7;
-    return this.statisticSeven;
+    const arr = [];
+    for (const key in this.statisticSeven){
+      if (this.statisticSeven.hasOwnProperty(key)){
+        arr.push(this.statisticSeven[key]);
+      }
+    }
+    this.chartOptions.series = arr;
   }
-
 
 }
