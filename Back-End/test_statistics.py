408f97b422bb6ffe868643477b162bdbfb89f53e
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-05-27 21:57:04 -0400

    tested percentage of people in each state top 10 populous states

--- Back-End/test_statistics.py
+++ Back-End/test_statistics.py
@@ -2,8 +2,8 @@ import unittest
 from statistics import Statistics
 import json
 
-
 first_json = "first_json_data.json"
+second_json = "second_json_data.json"
 
 
 def read_json(json_f):
@@ -30,6 +30,13 @@ class Test(unittest.TestCase):
         stat = Statistics(read_json(first_json), 'json')
         actual = stat.percentage_last_names_start_with_A_to_M_vs_N_to_Z()
         expected = 28.57
+        self.assertEqual(expected, actual, expected == actual)
+
+    def test_percentage_people_in_each_state_top_10_populous_states(self):
+        stat = Statistics(read_json(second_json), 'json')
+        actual = stat.percentage_people_in_each_state_top_10_populous_states()
+        expected = {"New York": 18.33, "Bayern": 16.66, "Galway": 15.00, "Baden": 13.33, "Rioja": 11.66,
+                    "Colorado": 8.33, "Marlborough": 6.66, "Ceuta": 5.00, "Bay": 3.33, "Brooklyn": 1.66}
         self.assertEqual(expected, actual, expected == actual)
 
 
