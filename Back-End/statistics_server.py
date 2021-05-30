3fbccd8a2779e8a1fdf1c7d24fe2476c525c2503
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-05-30 14:44:06 -0400

    Error checking, response to file types

--- Back-End/statistics_server.py
+++ Back-End/statistics_server.py
@@ -1,8 +1,6 @@
-import json
-from flask import Flask, request
+from flask import Flask, request, render_template
 from statistics import Statistics
 from flask_cors import CORS
-from time import sleep
 
 
 class FlaskServer:
@@ -11,20 +9,35 @@ class FlaskServer:
         self.service = Flask(__name__)
         CORS(self.service, resources={"/*": {"origins": ["*"]}})
         self.service.add_url_rule('/', view_func=self.home)
-        self.service.add_url_rule('/results', view_func=self.results, methods=['POST', 'GET'])
+        self.service.add_url_rule('/results', view_func=self.results, methods=['POST'])
+        self.service.add_url_rule('/results/<string:file_type>', view_func=self.results, methods=['POST'])
         self.service.run()
 
     def home(self):
-        return "HOME"
+        return render_template('home.html')
 
-    def results(self):
-        # TODO: remove sleep after development
-        # sleep(1)
-        print(request.headers)
-        print(request.data)
-        json_data = json.loads(request.data)
-        stats = Statistics(json_data, "json")
-        return stats.formatted_response()
+    def results(self, file_type: str = None):
+
+        response = {}
+
+        if request.content_type is None or request.content_type.lower() != 'application/json':
+            response['ERROR'] = 'ERROR', f'Expecting "application/json" value for Content-Type header'
+            return response, 400
+
+        file_types = ['txt', 'json', 'xml']
+        if file_type is not None and file_type.lower() not in file_types:
+            response['ERROR'] = f"Unrecognized file type. Only accepting {', '.join(file_types)}"
+            return response, 400
+        else:
+            if file_type is None:
+                # assign default file type of json
+                file_type = "json"
+            try:
+                stats = Statistics(request.json, file_type)
+                return stats.formatted_response()
+            except KeyError as e:
+                response['ERROR'] = f"JSON Key ERROR Detected {e.args}"
+                return response, 400
 
 
 if __name__ == '__main__':
